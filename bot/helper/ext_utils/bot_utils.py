43efc0e0397c8180c4329b023d40ccf239da64ab
Abdurrahman Aziz <115598902+Kirito-knight@users.noreply.github.com>
2022-10-26 11:11:30 +0300

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -118,7 +118,7 @@ def get_progress_bar_string(status):
     p = min(max(p, 0), 100)
     cFull = p // 8
     p_str = '⬢' * cFull
-    p_str += '⬡' * (12 - cFull)
+    p_str += '⬡' * (20 - cFull)
     p_str = f"[{p_str}]"
     return p_str
 
